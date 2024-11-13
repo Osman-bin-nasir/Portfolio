48b74212563d88c2256254eebdd32ef2667115bf
Osman-bin-nasir <osmanbinnasir19@gmail.com>
2024-11-13 20:54:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,3 +51,8 @@ function animate() {
 }
 
 animate();
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
